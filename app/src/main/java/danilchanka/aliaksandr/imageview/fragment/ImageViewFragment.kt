bba6a3bffe527679ec76ca9ad40dd66fb3cefa7c
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-28 17:43:02 +0200

    Create ImageViewFragment.kt

---
+++ app/src/main/java/danilchanka/aliaksandr/imageview/fragment/ImageViewFragment.kt
@@ -0,0 +1,23 @@
+package danilchanka.aliaksandr.imageview.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import danilchanka.aliaksandr.imageview.R
+
+class ImageViewFragment : Fragment() {
+
+    companion object {
+        fun newInstance(): ImageViewFragment = ImageViewFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_image_view, container, false)
+    }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }
+}
