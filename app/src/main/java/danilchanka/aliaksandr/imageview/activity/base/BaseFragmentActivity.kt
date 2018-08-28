2856055dba096a1a6a939e9c1050c062fa45d696
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-28 17:41:44 +0200

    Create class for activity work with fragment

---
+++ app/src/main/java/danilchanka/aliaksandr/imageview/activity/base/BaseFragmentActivity.kt
@@ -0,0 +1,28 @@
+package danilchanka.aliaksandr.imageview.activity.base
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentTransaction
+
+abstract class BaseFragmentActivity : BaseActivity() {
+
+    companion object {
+        const val DEFAULT_STACK = "DEFAULT_STACK"
+    }
+
+    fun addFragment(fragment: Fragment){
+        addFragment(fragment, true)
+    }
+
+    fun addFragment(fragment: Fragment, addToBackStack: Boolean){
+        val transaction = supportFragmentManager
+                .beginTransaction()
+                .add(getFragmentContainerId(), fragment)
+        if (addToBackStack) {
+            transaction.addToBackStack(DEFAULT_STACK)
+        }
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        transaction.commitAllowingStateLoss()
+    }
+
+    protected abstract fun getFragmentContainerId(): Int
+}
