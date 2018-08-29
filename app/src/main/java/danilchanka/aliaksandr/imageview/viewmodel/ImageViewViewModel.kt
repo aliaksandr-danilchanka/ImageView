eaf7b7ab0729af668f7530c36016755ddcdae4c1
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-29 19:00:20 +0200

    Subscription observable data

--- app/src/main/java/danilchanka/aliaksandr/imageview/viewmodel/ImageViewViewModel.kt
+++ app/src/main/java/danilchanka/aliaksandr/imageview/viewmodel/ImageViewViewModel.kt
@@ -1,17 +1,17 @@
 package danilchanka.aliaksandr.imageview.viewmodel
 
-import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.graphics.Bitmap
 import android.view.View
 import danilchanka.aliaksandr.imageview.api.RestHelper
 import danilchanka.aliaksandr.imageview.util.Utils
+import danilchanka.aliaksandr.imageview.viewmodel.base.BaseLoadingViewModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 
-class ImageViewViewModel : ViewModel() {
+class ImageViewViewModel : BaseLoadingViewModel() {
 
     var login: String = "danilchanka"
     var password: String = "aliaksandr"
@@ -20,23 +20,25 @@ class ImageViewViewModel : ViewModel() {
 
     fun onSubmitClick(view: View) {
         isLoading.set(true)
-        val apiService = RestHelper.create()
-        apiService.getImageBase64Encoded(login, Utils.stringToSha1(password))
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({ result ->
-                    imageBitmap.set(Utils.stringBase64ToBitmap(result.image))
-                    isLoading.set(false)
-                }, { error ->
-                    error.printStackTrace()
-                })
+        addSubscription(
+                RestHelper.getRestInterface()
+                        .getImageBase64Encoded(login, Utils.stringToSha1(password))
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({ result ->
+                            imageBitmap.set(Utils.stringBase64ToBitmap(result.image))
+                            isLoading.set(false)
+                        }, { error ->
+                            error.printStackTrace()
+                        })
+        )
     }
 
-    fun changeLogin(s: CharSequence){
+    fun changeLogin(s: CharSequence) {
         login = s.toString()
     }
 
-    fun changePassword(s: CharSequence){
+    fun changePassword(s: CharSequence) {
         password = s.toString()
     }
 }
