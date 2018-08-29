88eb0f3d85d9a500eeb1bb1b159759e707e6cfa7
Aliaksandr Danilchanka <danilali@fel.cvut.cz>
2018-08-29 16:37:57 +0200

    Add convert functions

--- app/src/main/java/danilchanka/aliaksandr/imageview/util/Utils.kt
+++ app/src/main/java/danilchanka/aliaksandr/imageview/util/Utils.kt
@@ -1,16 +1,32 @@
 package danilchanka.aliaksandr.imageview.util
 
-import android.app.Activity
-import android.view.View
-import android.view.WindowManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Base64
+import java.math.BigInteger
+import java.security.MessageDigest
+
 
 class Utils {
 
     companion object {
-        fun requestFocus(view: View, activity: Activity?) {
-            if (view.requestFocus() && activity != null) {
-                activity.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+
+        fun stringToSha1(input: String): String {
+            var sha1 = ""
+            try {
+                val digest = MessageDigest.getInstance("SHA-1")
+                digest.reset()
+                digest.update(input.toByteArray(Charsets.UTF_8))
+                sha1 = String.format("%040x", BigInteger(1, digest.digest()))
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
+            return sha1
+        }
+
+        fun stringBase64ToBitmap(encodedImage: String): Bitmap{
+            val decodedString = Base64.decode(encodedImage, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
         }
     }
 
